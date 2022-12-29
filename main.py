824b6e28c8a404dde9d7fb80745648bb7f51938a
Emilio C <emilio.cuandu@gmail.com>
2022-12-29 17:19:00 +0000

    Intro to game, user adds name, age and location

---
+++ main.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter where you're from: ")
+
+print(
+    "Hello {name}, you are a  {age} year old Pokemon trainer from {location}.".format(name=name, age=age, location=location)
+    )
+print("My name is Oak, but everyone calls me the professor. I'm a leading expert on Pokemon and I research them with a device called the Pokedex.")
+print("You're about to start your very own adventure.")
+print(".........")
+print("It seems this Pikachu has taken a liking to you and wants to join you on your adventure.")
+print("In order to get stronger you'll need to catch and train more pokemon.")
+print("Let's go and catch your very first Pokemon!")
